f1de96b811d046188b5600225ab98e83405a3360
MikyVaz <miguelvazquezdelarubia@gmail.com>
2025-02-24 16:47:16 +0100

    caritas

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
+//Constantes
 const express = require("express");
-
 const app = express();
 const PORT = 16078;
+const cool = require("cool-ascii-faces");
+//Codigo
 app.get("/",(request, response) => {
-    response.send("Hello from the server!");
-})
+    response.send("Hi Server!");
+});
+app.get("/cool",(request, response) => {
+    response.send(cool());
+});
 
 app.listen(PORT,() =>{
     console.log(`Server running on port ${PORT}!`);
