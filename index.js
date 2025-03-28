2773a48746bcaf68095bf35de69d57a85da7b817
MikyVaz <miguelvazquezdelarubia@gmail.com>
2025-03-28 11:49:27 +0100

    jf

--- index.js
+++ index.js
@@ -1,51 +1,17 @@
 //Constantes
-const express = require("express");
+// const express = require("express");
+
+import express from "express"; // las dos son las mismas maneras de importar pero escritas de forma diferente. 
+import { loadBackend } from "./src/back/index.js"; 
+
 const app = express();
 const PORT = process.env.PORT || 16078;
-const cool = require("cool-ascii-faces");
-const BASE_API = "/api/v1";
-app.use("/", express.static("./public"));
+
 app.use(express.json());
+app.use("/", express.static("./public"));
 
-let contacts = [
-    {
-        name: "peter",
-        phone: 123456
-    },
-    {
-        name: "pablo",
-        phone: 789102
-    },
-    {
-        name: "sara",
-        phone: 5873420
-    }
-]
+loadBackend(app);
 
-//Codigo
-app.get("/",(request, response) => {
-    response.send("Love u Sarita la brauni!!");
-});
-app.get(BASE_API + "/contacts",(request, response) => {
-    console.log("Llamando al GET to /contacts");
-    response.send(JSON.stringify(contacts));
-});
-
-app.post(BASE_API + "/contacts",(request, response) => {
-    console.log("Llamando al POST to /contacts");
-    //console.log(`<${request.body}>`)
-    let newContact = request.body;
-    
-    contacts.push(newContact);
-    response.sendStatus(201);
-
-});
-
-app.use("/guay", express.static("./html/index.html"))
 app.listen(PORT,() =>{
     console.log(`Server running on port ${PORT}!`);
 });
-
-
-
-console.log(`Inicializando servidor... `);
---
+++ src/back/index.js
@@ -0,0 +1,77 @@
+import dataStore from "nedb";
+const BASE_API = "/api/v1";
+let db = new dataStore();
+let initialContacts = [
+    {
+        name: "peter",
+        phone: 123456
+    },
+    {
+        name: "pablo",
+        phone: 789102
+    },
+    {
+        name: "sara",
+        phone: 5873420
+    }
+]
+db.find({}, (err, contacts)=>{
+    if (contacts.length < 1){
+        db.insert(initialContacts); //faltaría tratar los errores y eso. 
+
+    }
+}); // buc
+function loadBackend (app){
+    app.get(BASE_API+"/contacts",(request,response)=>{
+        console.log("New GET to /contacts");
+        response.send(JSON.stringify(initialContacts));        
+        db.find({},(err,contacts)=>{
+            response.send(JSON.stringify(contacts.map((c)=>{
+                delete c._id;
+                return c;
+            }),null,2));
+        });
+
+    });
+    
+    
+    app.post(BASE_API+"/contacts",(request,response)=>{
+    
+        console.log("POST to /contacts");
+        console.log(`<${request.body}>`);
+    
+        let newContact = request.body;
+        
+        db.insert(newContact);
+    
+        response.sendStatus(201);
+    });
+   
+    app.delete(BASE_API+"/contacts/:name",(request,response)=>{
+    
+        let name = request.params.name;
+        console.log(`DELETE to /contacts/${name}`);
+            
+        db.remove({ "name" : name},{},(err,numRemoved)=>{
+            if(err){
+                response.status(500).send("Error code 01 (please contact admin)");                
+                console.error(`ÈRROR: ${err}`);
+            }else{
+                if(numRemoved >= 1){
+                    response.sendStatus(200);
+                }else{
+                    response.sendStatus(404);
+                }
+            }
+        });
+    
+    });
+   
+
+
+
+}
+
+
+
+export {loadBackend }; // cuando haga un require podré utilizar estos elementos. 
